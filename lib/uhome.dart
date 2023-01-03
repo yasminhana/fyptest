8cc23b1b7d0816f040dbf16a3b06968c2832c4a3
yasminhana <yasmin.hana@live.iium.edu.my>
2023-01-03 23:27:49 +0800

    fyp

--- lib/uhome.dart
+++ lib/uhome.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 // import 'package:fyptest/home.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'routes.dart';
 //import 'package:firebase_auth/firebase_auth.dart';
 
@@ -11,22 +12,35 @@ class Uhome extends StatefulWidget {
 }
 
 class _UhomeState extends State<Uhome> {
+  late Map<DateTime, List<Event>> selectedEvents;
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
+  final TextEditingController _eventController = TextEditingController();
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _eventController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: NavDrawer(),
       appBar: AppBar(
-        centerTitle: true,
-        title: const Text('Homepage'),
-      ),
-      body: Center(
-          child: Container(
-        padding: const EdgeInsets.all(8),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
+          centerTitle: true,
+          title: const Text('Homepage'),
+          actions: <Widget>[
             Padding(
-                padding: const EdgeInsets.all(5),
+                padding: const EdgeInsets.only(right: 20.0),
                 child: ElevatedButton(
                     child: const Text('LOGIN FOR ADMIN'),
                     onPressed: () {
@@ -35,18 +49,102 @@ class _UhomeState extends State<Uhome> {
                         Routes.login,
                       );
                     })),
-            Container(
-              child: Text('ANNOUNCEMENTS'),
-              decoration:
-                  BoxDecoration(shape: BoxShape.rectangle, color: Colors.green),
-              padding: EdgeInsets.all(70.0),
-              margin: EdgeInsets.all(15.0),
-            )
-          ],
+          ]),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    height: 150,
+                    width: 300,
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    decoration:
+                        BoxDecoration(border: Border.all(color: Colors.black)),
+                    child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Center(
+                          child: Text(
+                            'ANNOUNCEMENTS' * 20,
+                            style: const TextStyle(fontSize: 15),
+                          ),
+                        ))),
+                Container(
+                  width: 500,
+                  margin: const EdgeInsets.all(10),
+                  padding: const EdgeInsets.all(10),
+                  child: TableCalendar(
+                    focusedDay: selectedDay,
+                    firstDay: DateTime(2020),
+                    lastDay: DateTime(2050),
+                    calendarFormat: format,
+                    onFormatChanged: (CalendarFormat _format) {
+                      setState(() {
+                        format = _format;
+                      });
+                    },
+                    startingDayOfWeek: StartingDayOfWeek.sunday,
+                    daysOfWeekVisible: true,
+
+                    //Day Changed
+                    onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                      setState(() {
+                        selectedDay = selectDay;
+                        focusedDay = focusDay;
+                      });
+                      print(focusedDay);
+                    },
+                    selectedDayPredicate: (DateTime date) {
+                      return isSameDay(selectedDay, date);
+                    },
+
+                    //To style the Calendar
+                    calendarStyle: const CalendarStyle(
+                      isTodayHighlighted: true,
+                      selectedDecoration: BoxDecoration(
+                        color: Colors.blue,
+                        shape: BoxShape.circle,
+                      ),
+                      selectedTextStyle: TextStyle(color: Colors.white),
+                      todayDecoration: BoxDecoration(
+                        color: Colors.purpleAccent,
+                        shape: BoxShape.circle,
+                      ),
+                      defaultDecoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      weekendDecoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                    headerStyle: HeaderStyle(
+                      formatButtonVisible: false,
+                      titleCentered: true,
+                      formatButtonShowsNext: false,
+                      formatButtonDecoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(5.0),
+                      ),
+                      formatButtonTextStyle: const TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ]),
         ),
-      )),
+      ),
     );
   }
+}
+
+class Event {
+  final String title;
+  Event({required this.title});
+
+  String toString() => this.title;
 }
 
 class NavDrawer extends StatelessWidget {
