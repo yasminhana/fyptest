f7d14bbb1ef255553e0dd9bdeb1edcde5c5d3700
yasminhana <yasmin.hana@live.iium.edu.my>
2023-01-03 17:51:48 +0800

    fyp

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-//import 'package:fyptest/home.dart';
+import 'package:table_calendar/table_calendar.dart';
 import 'routes.dart';
-//import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/src/material/floating_action_button.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,8 +12,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  late Map<DateTime, List<Event>> selectedEvents;
+  DateTime today = DateTime.now();
+
+  TextEditingController _eventController= TextEditingController();
+
+  void _onDaySelected(DateTime day, DateTime focusedDay) {
+    setState(() {
+      today = day;
+    });
+  }
+
+  @override
+  void initState() {
+    selectedEvents = {};
+    super.initState();
+  }
+
+  List<Event> _getEventsfromDay(DateTime date) {
+    return selectedEvents[date] ?? [];
+  }
+
+  @override
+  void dispose(){
+    _eventController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    dynamic eventLoader;
     return Scaffold(
       drawer: NavDrawer(),
       appBar: AppBar(
@@ -21,7 +50,7 @@ class _HomeState extends State<Home> {
       ),
       body: Center(
           child: Container(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
@@ -32,18 +61,62 @@ class _HomeState extends State<Home> {
               padding: EdgeInsets.all(70.0),
               margin: EdgeInsets.all(15.0),
             ),
+            Text("Selected Day = " + today.toString().split(" ")[0]),
             Container(
-              child: Text('Calendar'),
-              decoration:
-                  BoxDecoration(shape: BoxShape.rectangle, color: Colors.green),
-              padding: EdgeInsets.all(70.0),
-              margin: EdgeInsets.all(15.0),
-            )
+              child: TableCalendar(
+                locale: "en_US",
+                rowHeight: 43,
+                headerStyle: HeaderStyle(
+                    formatButtonVisible: false, titleCentered: true),
+                availableGestures: AvailableGestures.all,
+                selectedDayPredicate: (day) => isSameDay(day, today),
+                focusedDay: today,
+                firstDay: DateTime.utc(2022, 1, 1),
+                lastDay: DateTime.utc(2023, 12, 31),
+                onDaySelected: _onDaySelected,
+              ),
+            ),
+            eventLoader: _getEventsfromDay,
+            floatingActionButton: FloatingActionButton.extended(
+              onPressed: () => showDialog(
+                context: context, 
+                builder: (context) =>AlertDialog(
+                  title:Text("Add Event"),
+                  content:TextFormField(
+                    controller: _eventController,
+                  ),
+                  actions:[
+                    TextButton(
+                      child: Text("Cancel"),
+                      onPressed: ()=> Navigator.pop(context),
+                    ),
+                    TextButton(
+                      child: Text("Ok"),
+                      onPressed: (){
+                        if(_eventController.text.isEmpty){
+                          Navigator.pop(context);
+                          return;
+                        }
+                      },
+                    ),
+                  ],
+                ),
+              ), 
+              label: Text:("Add Event"),
+              icon: Icon(Icons.add),
+            ),
           ],
         ),
       )),
     );
   }
+}
+
+class Event {
+  final String title;
+  Event({required this.title});
+
+  String toString() => this.title;
 }
 
 class NavDrawer extends StatelessWidget {
