ff7a049468c8e8c380c50d3f39d33a9db6c2052f
yasminhana <yasmin.hana@live.iium.edu.my>
2023-01-04 23:42:43 +0800

    fyp

--- lib/uhome.dart
+++ lib/uhome.dart
@@ -34,109 +34,110 @@ class _UhomeState extends State<Uhome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: NavDrawer(),
-      appBar: AppBar(
-          centerTitle: true,
-          title: const Text('Homepage'),
-          actions: <Widget>[
-            Padding(
-                padding: const EdgeInsets.only(right: 20.0),
-                child: ElevatedButton(
-                    child: const Text('LOGIN FOR ADMIN'),
-                    onPressed: () {
-                      Navigator.pushNamed(
-                        context,
-                        Routes.login,
-                      );
-                    })),
-          ]),
-      body: Center(
-        child: Container(
-          padding: const EdgeInsets.all(8),
-          child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                    height: 150,
-                    width: 300,
-                    margin: const EdgeInsets.all(10),
-                    padding: const EdgeInsets.all(10),
-                    decoration:
-                        BoxDecoration(border: Border.all(color: Colors.black)),
-                    child: SingleChildScrollView(
-                        scrollDirection: Axis.vertical,
-                        child: Center(
-                          child: Text(
-                            'ANNOUNCEMENTS' * 20,
-                            style: const TextStyle(fontSize: 15),
+        drawer: NavDrawer(),
+        appBar: AppBar(
+            centerTitle: true,
+            title: const Text('Homepage'),
+            actions: <Widget>[
+              Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: ElevatedButton(
+                      child: const Text('Login'),
+                      onPressed: () {
+                        Navigator.pushNamed(
+                          context,
+                          Routes.login,
+                        );
+                      })),
+            ]),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Center(
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                        height: 150,
+                        width: 300,
+                        margin: const EdgeInsets.all(10),
+                        padding: const EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            border: Border.all(color: Colors.black)),
+                        child: SingleChildScrollView(
+                            scrollDirection: Axis.vertical,
+                            child: Center(
+                              child: Text(
+                                'ANNOUNCEMENTS' * 20,
+                                style: const TextStyle(fontSize: 15),
+                              ),
+                            ))),
+                    Container(
+                      width: 500,
+                      margin: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(10),
+                      child: TableCalendar(
+                        focusedDay: selectedDay,
+                        firstDay: DateTime(2020),
+                        lastDay: DateTime(2050),
+                        calendarFormat: format,
+                        onFormatChanged: (CalendarFormat _format) {
+                          setState(() {
+                            format = _format;
+                          });
+                        },
+                        startingDayOfWeek: StartingDayOfWeek.sunday,
+                        daysOfWeekVisible: true,
+
+                        //Day Changed
+                        onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                          setState(() {
+                            selectedDay = selectDay;
+                            focusedDay = focusDay;
+                          });
+                          print(focusedDay);
+                        },
+                        selectedDayPredicate: (DateTime date) {
+                          return isSameDay(selectedDay, date);
+                        },
+
+                        //To style the Calendar
+                        calendarStyle: const CalendarStyle(
+                          isTodayHighlighted: true,
+                          selectedDecoration: BoxDecoration(
+                            color: Colors.blue,
+                            shape: BoxShape.circle,
                           ),
-                        ))),
-                Container(
-                  width: 500,
-                  margin: const EdgeInsets.all(10),
-                  padding: const EdgeInsets.all(10),
-                  child: TableCalendar(
-                    focusedDay: selectedDay,
-                    firstDay: DateTime(2020),
-                    lastDay: DateTime(2050),
-                    calendarFormat: format,
-                    onFormatChanged: (CalendarFormat _format) {
-                      setState(() {
-                        format = _format;
-                      });
-                    },
-                    startingDayOfWeek: StartingDayOfWeek.sunday,
-                    daysOfWeekVisible: true,
-
-                    //Day Changed
-                    onDaySelected: (DateTime selectDay, DateTime focusDay) {
-                      setState(() {
-                        selectedDay = selectDay;
-                        focusedDay = focusDay;
-                      });
-                      print(focusedDay);
-                    },
-                    selectedDayPredicate: (DateTime date) {
-                      return isSameDay(selectedDay, date);
-                    },
-
-                    //To style the Calendar
-                    calendarStyle: const CalendarStyle(
-                      isTodayHighlighted: true,
-                      selectedDecoration: BoxDecoration(
-                        color: Colors.blue,
-                        shape: BoxShape.circle,
-                      ),
-                      selectedTextStyle: TextStyle(color: Colors.white),
-                      todayDecoration: BoxDecoration(
-                        color: Colors.purpleAccent,
-                        shape: BoxShape.circle,
-                      ),
-                      defaultDecoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                      ),
-                      weekendDecoration: BoxDecoration(
-                        shape: BoxShape.circle,
+                          selectedTextStyle: TextStyle(color: Colors.white),
+                          todayDecoration: BoxDecoration(
+                            color: Colors.purpleAccent,
+                            shape: BoxShape.circle,
+                          ),
+                          defaultDecoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          weekendDecoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                        ),
+                        headerStyle: HeaderStyle(
+                          formatButtonVisible: false,
+                          titleCentered: true,
+                          formatButtonShowsNext: false,
+                          formatButtonDecoration: BoxDecoration(
+                            color: Colors.blue,
+                            borderRadius: BorderRadius.circular(5.0),
+                          ),
+                          formatButtonTextStyle: const TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
-                    headerStyle: HeaderStyle(
-                      formatButtonVisible: false,
-                      titleCentered: true,
-                      formatButtonShowsNext: false,
-                      formatButtonDecoration: BoxDecoration(
-                        color: Colors.blue,
-                        borderRadius: BorderRadius.circular(5.0),
-                      ),
-                      formatButtonTextStyle: const TextStyle(
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ),
-              ]),
-        ),
-      ),
-    );
+                  ]),
+            ),
+          ),
+        ));
   }
 }
 
