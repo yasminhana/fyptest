b1c7f4f13d8ca524d8c7e6aff221657a1e5107b3
yasminhana <yasmin.hana@live.iium.edu.my>
2023-01-06 22:23:47 +0800

    fyp

--- lib/uresult.dart
+++ lib/uresult.dart
@@ -12,9 +12,42 @@ class _UresultState extends State<Uresult> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Result Page'),
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Result Page'),
+        ),
+        body: Center(
+            child: ListView(children: <Widget>[
+          const Center(
+              child: Text(
+            'Result',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          )),
+          DataTable(
+            columns: const [
+              DataColumn(
+                  label: Text('Rank',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))),
+              DataColumn(
+                  label: Text('Name',
+                      style: TextStyle(
+                          fontSize: 18, fontWeight: FontWeight.bold))),
+            ],
+            rows: const [
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('yasmin hana')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('2')),
+                DataCell(Text('aisyah')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('3')),
+                DataCell(Text('dibqib')),
+              ]),
+            ],
+          ),
+        ])));
   }
 }
